d1b3afe903dff21b4ca34cd465966267ad8b643d
Eumie Jhong <eumiejhong@eumies-mbp-2.lan>
2021-07-18 19:26:12 -0400

    adding grocery items exercise

---
+++ Unit 32/Unit 32.2 Exercise/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const ExpressError = require('./expressError');
+
+app.get('/items', function(req, res, next) {
+    return new ExpressError("Not Found", 404);
+});
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    return res.json({
+        error: err.message,
+    });
+});
+
+module.exports = app
