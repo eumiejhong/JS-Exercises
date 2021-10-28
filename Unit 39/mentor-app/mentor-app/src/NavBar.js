25340fe995f9d99c16f5c58f6c4ef59356a6b356
Eumie Jhong <eumiejhong@eumies-mbp-2.lan>
2021-10-27 21:56:21 -1000

    added hamburger icon

--- Unit 39/mentor-app/mentor-app/src/NavBar.js
+++ Unit 39/mentor-app/mentor-app/src/NavBar.js
@@ -1,22 +1,52 @@
-import React from "react";
-import { Link } from "react-router-dom"
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+const HamburgerMenu = ({ open, toggle, hover, bold, unhover }) => {
+    const className = 'hamburger' + (open ? ' open' : '');
+    const hoverName = 'hamburgerLine' + (bold ? ' bold' : '')
+
+    return <div className={className} onClick={toggle} onMouseEnter={hover} onMouseLeave={unhover}>
+        <div className={hoverName}></div>
+        <div className={hoverName}></div>
+        <div className={hoverName}></div>
+    </div>
+}
+
 
 export default function NavBar() {
+    const [isHidden, setHidden] = useState("true");
+    const [isHover, setHover] = useState("true");
+
+    const handleToggle = () => {
+        setHidden(!isHidden);
+    };
+
+    const handleMouseOver = () => {
+        setHover(true);
+    };
+
+    const handleMouseOut = () => {
+        setHover(false);
+    };
 
     return (
+        <>
+            <HamburgerMenu toggle={handleToggle} open={!isHidden} hover={handleMouseOver} unhover={handleMouseOut} bold={isHover} />
+            <div className={isHidden ? "hidden" : null}>
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/About">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/Contact">Contact us</Link>
+                    </li>
+                </ul>
+            </div>
 
-        <div>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/About">About</Link>
-                </li>
-                <li>
-                    <Link to="/Contact">Contact us</Link>
-                </li>
-            </ul>
-        </div>
+        </>
     )
 };
